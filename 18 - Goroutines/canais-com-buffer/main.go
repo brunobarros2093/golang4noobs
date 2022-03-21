b2b92bf79548d40a7f851c98830268d4f9d12986
Bruno Barros <brunog.barros@pm.me>
2022-03-21 20:39:04 -0300

    18 - GoRoutines - Canais com Buffer

--- 18 - Goroutines/canais-com-buffer/main.go
+++ 18 - Goroutines/canais-com-buffer/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	// é a mesma instanciação de um buffer comum, apenas dizemos o numero de canais que serão abertos
+	// dessa forma o Go sabe que abriremos dois canais
+	// o canal só bloqueará quando o buffer chegar no limite
+	canal := make(chan string, 2)
+	canal <- "Ola mundo!"
+	canal <- "Ola mundo 2!"
+
+	mensagem := <-canal
+	fmt.Println(mensagem)
 }
