c8614ccfb24f0b2d6284ce707a16e20486439249
Bruno Barros <brunog.barros@pm.me>
2022-03-21 21:21:58 -0300

    20.2 - Generators

--- 20 - Padroes de concorrencia/20.2 - Generators/main.go
+++ 20 - Padroes de concorrencia/20.2 - Generators/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+func main() {
+	// A Função já retorna um canal e já escreve de forma assincrona, por isso não há necessidade de
+	// utilizar o 'go' aqui na frente - afinal, ele já é concorrente e já retorna um canal
+	canal := escrever("Ola canal generator")
+	for i := 0; i <= 10; i++ {
+		fmt.Println(<-canal)
+	}
+}
+
+// função generator - gera um canal assincrono
+// retorna um canal de escrita (só recebe)
+func escrever(texto string) <-chan string {
+	canal := make(chan string)
+
+	go func() {
+		for {
+			canal <- fmt.Sprintf("Valor recebido %s", texto)
+			time.Sleep(time.Millisecond * 500)
+		}
+	}()
+
+	return canal
 }
