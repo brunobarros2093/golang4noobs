72e9b1bcdaf4814411f8137cdae8ed3aac0bd835
Bruno Barros <brunog.barros@pm.me>
2022-03-24 21:36:16 -0300

    25 - Conexão com banco, querys etc

---
+++ 25 - Banco de Dados/go.mod
@@ -0,0 +1,5 @@
+module banco-de-dados
+
+go 1.18
+
+require github.com/go-sql-driver/mysql v1.6.0 // indirect
---
+++ 25 - Banco de Dados/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+)
+
+func main() {
+	stringConn := "golang:golang@/devbook?charset=utf8&parseTime=True&loc=Local"
+	db, err := sql.Open("mysql", stringConn)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func(db *sql.DB) {
+		_ = db.Close()
+	}(db)
+
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Conexão aberta com sucesso!")
+
+	linhas, err := db.Query("select * from usuarios")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func(linhas *sql.Rows) {
+	_:
+		linhas.Close()
+
+	}(linhas)
+	// retorna em bytes
+	fmt.Println(linhas)
+}
