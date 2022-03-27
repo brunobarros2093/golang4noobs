1fd0106dcb95d9c31b7c0785c77940f385141e67
Bruno Barros <brunog.barros@pm.me>
2022-03-27 13:00:22 -0300

    Update com as novas funções dos genericos

--- 16 - genericos/main.go
+++ 16 - genericos/main.go
@@ -11,8 +11,31 @@ func generica(interf interface{}) {
 type forma interface {
 }
 
+// type parameter
+// type inferance
+// type Set
+//---------\/ = faz o mesmo que utilizar interface{}
+func min[T any](a T, b T) T {
+	return a
+}
+
+type minTypes interface {
+	~float64 | int
+}
+
+//-----------\/ type Sets
+func max[T minTypes](a T) T {
+	return a
+}
+
 func main() {
+	fmt.Println("--- GENERICOS ANTES DA VERSÃO 1.18 -- UTILIZANDO INTERFACE")
 	generica("Sou uma String")
 	generica(123)
 	generica(true)
+
+	fmt.Println("--- GENERICOS APÓS DA VERSÃO 1.18 -- UTILIZANDO UM TIPO GENERICO T interface como parametro")
+	//--------------\/ opcional
+	fmt.Println(min[int](1, 2))
+	fmt.Println(min("bruno", "birros"))
 }
